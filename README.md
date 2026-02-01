9d8a0555e8adc3f26d398eea65283470490d671c
Graylag-PD <59648689+Graylag-PD@users.noreply.github.com>
2026-02-02 00:40:18 +0100

    installation script enhancement

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Instalation for automated management by moonraker.
+# Automated instalation with an automated management by moonraker.
 
 SSH into your printer and run following code
 ```
@@ -7,17 +7,44 @@ bash ~/goose_purge_macro/install.sh
 
 ```
 
-Add following lines to moonraker.conf:
+Afterwards add `[include goose_purge/goose_purge.cfg]` to printer.cfg and restart the klipper
+
+# Manual instalation.
+For advanced users who would like to modify any of the parameters (e.g. file locations) or for less common configurations where automated script fails  
+  
+SSH into your printer and run following code. This will download a fresh copy of the repository. You should skip this step in case you already cloned repo before.  
+`git clone https://github.com/Graylag-PD/Goose-Purge-Macro.git goose_purge_macro`  
+  
+Now you need to copy two configuration files.  
+Run following command to create a symbolic link of the macro core file. This also deletes any previously existing file of this name.  
+Symbolic link means this file remains in the original folder and only gets "mirrored" into the configuration folder. This ensures it gets updated automaticaly, but also provides write protection  when opened from Mainsail or Fluidd.  
+`ln -sf ~/goose_purge_macro/goose_purge_core.cfg ~/printer_data/config/goose_purge_core.cfg`  
+  
+Next run following command to copy the user configuration file. Note, that this command does not overwrite any preexisting file of the same name.  
+`cp --update=none ~/goose_purge_macro/goose_purge.cfg ~/printer_data/config/goose_purge.cfg`  
+Note, that you can replace source file `goose_purge.cfg` for `goose_purge-dcmot.cfg` or `goose_purge-stpmot.cfg`. Those are prefiltered configurations depending on whether you want to use DC or stepper motor. Resulting commands would like like this:  
+`cp --update=none ~/goose_purge_macro/goose_purge-dcmot.cfg ~/printer_data/config/goose_purge.cfg`  
+or  
+`cp --update=none ~/goose_purge_macro/goose_purge-stpmot.cfg ~/printer_data/config/goose_purge.cfg`  
+NOTE: On some older printers (pre 2022) you may not have your configuration files in folder `~/printer_data/config/`. If this is your case, modify those commands before using them.  
+  
+Next step is to symlink the python module into the extras folder. Run following command:  
+`ln -sf ~/goose_purge_macro/goose_purge.py ~/klipper/klippy/extras/goose_purge.py`  
+in some cases this may fail due to insufficient user privileges. If that is your case, use this instead  
+`sudo ln -sf ~/goose_purge_macro/goose_purge.py ~/klipper/klippy/extras/goose_purge.py`  
+  
+In the next step you are going to configure the Moonraker Update Manager for automated updating.  
+Open the `moonraker.cfg` file and add following lines:  
 ```
-[update_manager goose_purge_macro]
+[update_manager goose_purge]
 type: git_repo
 primary_branch: v0-dev
 path: ~/goose_purge_macro
 origin: https://github.com/Graylag-PD/Goose-Purge-Macro.git
 managed_services: klipper
 ```
-
-Add `[include goose_purge/goose_purge.cfg]` to printer.cfg
+  
+Last step is to add `[include goose_purge/goose_purge.cfg]` to printer.cfg and restart the klipper
 
 # Manual update
 Following commands will delete your `goose_purge_core.cfg` and create new symlink for the freshly pulled file. The `goose_purge.cfg` will be preserved if exists and added if not.  
@@ -33,7 +60,7 @@ You have several options:
 ## Quick and dirty
 Copy the `goose_purge.cfg` into the config folder. You can use following command:
 ```
-sudo cp -f ~/goose_purge_macro/goose_purge_core.cfg ~/printer_data/config/goose_purge/goose_purge_core.cfg
+cp -f ~/goose_purge_macro/goose_purge_core.cfg ~/printer_data/config/goose_purge_core.cfg
 ```
 Note, that this will disable the updating mechanism. Moonraker will still pull newest files and show the macro to be updated, however your file in config folder will not be changed  
 
--- install.sh
+++ install.sh
@@ -1,26 +1,97 @@
 #!/bin/bash
-
+#
 echo "Goose Purge Macro installation script - installing"
-cd ~
-ln -sf ~/goose_purge_macro/goose_purge_core.cfg ~/printer_data/config/goose_purge_core.cfg
-ln -sf ~/goose_purge_macro/goose_purge.py ~/klipper/klippy/extras/goose_purge.py
-if [ -f ~/printer_data/config/goose_purge.cfg ]; then
-	echo "Configuration file already exists and will not be overwritten"
+echo "------------------------------------------------------------"
+#
+echo "Installing configuration files"
+KLIPPER_CONF_DIR="${HOME}/printer_data/config"
+# check for folder location
+if [ ! -d "$KLIPPER_CONF_DIR" ]; then
+    echo "New structure folder ~/printer_data not found, looking for a legacy structure..."
+    
+    # locations to be checked
+    if [ -d $HOME/klipper_config ]; then
+        KLIPPER_CONF_DIR="${HOME}/klipper_config"
+    elif [ -d $HOME/printer_config ]; then
+        KLIPPER_CONF_DIR="${HOME}/printer_config"
+    else
+        KLIPPER_CONF_DIR="${HOME}"
+    fi
+fi
+echo "Configuration files will be copied to: $KLIPPER_CONF_DIR"
+ln -sf goose_purge_core.cfg $KLIPPER_CONF_DIR/goose_purge_core.cfg
+echo ""
+
+# 2 copy the configuration file
+# no need to repeate the search for the correct folder
+if [ -f $KLIPPER_CONF_DIR/goose_purge.cfg ]; then
+	echo "User configuration file already exists and will not be overwritten"
 	echo "If you would like to restore a default configuration, please delete or rename the goose_purge.cfg in the configuration folder and then run the installation script again"
 else
-	echo "Configuration file not found, default file will be copied to your configuration folder"
+	echo "User configuration file not found, default file will be copied to your configuration folder"
 	echo "What purger type would you like to configure?"
 	echo "	1 - Belt purge with DC motor"
 	echo "	2 - Belt purge with Stepper motor"
 	read -p "Purger type: " purger_type
 	if [ $purger_type -eq 1 ]; then
-		cp --update=none ~/goose_purge_macro/goose_purge-dcmot.cfg ~/printer_data/config/goose_purge.cfg
+		cp --update=none goose_purge-dcmot.cfg $KLIPPER_CONF_DIR/goose_purge.cfg
 	elif [ $purger_type -eq 2 ]; then
-		cp --update=none ~/goose_purge_macro/goose_purge-stpmot.cfg ~/printer_data/config/goose_purge.cfg
+		cp --update=none goose_purge-stpmot.cfg $KLIPPER_CONF_DIR/goose_purge.cfg
 	else
 		echo "Invalid selection, configuration file not copied. Copy the correct file manually or run the installation script again and select the valid value"
 		echo ""
 	fi
 fi
-echo "Installation script completed. In case of doubt you can run it again at any time, your configuration file will never get overwritten!"
+echo ""
+
+# 3 copy the python module
+echo "Installing klipper module"
+EXTRAS_DIR="${HOME}/klipper/klippy/extras"
+# check for existence and write permissions
+if [ -d $EXTRAS_DIR ]; then
+    if [ -w $EXTRAS_DIR ]; then
+        ln -sf goose_purge.py $EXTRAS_DIR/goose_purge.py
+    else
+        # ask for sudo if we don't have write permissions
+        sudo ln -sf goose_purge.py $EXTRAS_DIR/goose_purge.py
+    fi
+else
+    echo "Error: Klipper extras folder not found"
+	echo "Installation failed"
+    exit 1
+fi
+echo ""
+
+# 4 add record to moonraker.cfg
+echo "Configuring Moonraker update manager"
+MOONRAKER_CONF="$KLIPPER_CONF_DIR/moonraker.conf"
+SECTION_NAME="[update_manager goose_purge]"
+
+if grep -qF "$SECTION_NAME" "$MOONRAKER_CONF"; then
+    echo "Update manager in moonraker.conf already set"
+else
+    echo "Adding Update Manager configuration to moonraker.conf..."
+    cat << EOF >> "$MOONRAKER_CONF"
+
+$SECTION_NAME
+type: git_repo
+primary_branch: v0-dev
+path: ~/goose_purge_macro
+origin: https://github.com/Graylag-PD/Goose-Purge-Macro.git
+managed_services: klipper
+EOF
+fi
+
+
+
+
+
+
+echo -e "\n------------------------------------------------------------"
+echo "Installation script completed."
+echo "Make sure to add following line to your printer.cfg:"
+echo -e "\e[1;32m[include goose_purge.cfg]\e[0m"
 echo "Don't forget to restart the Klipper before you can start using this macro"
+echo -e "\nIn case of doubt you can run this script again at any time,"
+echo "your configuration file will never get overwritten!"
+echo "------------------------------------------------------------"
